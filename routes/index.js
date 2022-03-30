807eac8680e3ed55618a59870ecf15004a0ee196
sammy-soto10 <73593202+sammy-soto10@users.noreply.github.com>
2022-03-29 21:18:37 -0400

    backend login and register set up

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('login')
+})
+
+router.get('/home', (req, res) => {
+    res.render('home')
+})
+
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
+module.exports = router
