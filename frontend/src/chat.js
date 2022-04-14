c219dc2d152d72519d5ba985194490656af1d047
Ksenia Velichko <ksushi@Ksenias-MacBook-Pro.local>
2022-04-14 14:55:01 -0400

    my commit

---
+++ frontend/src/chat.js
@@ -0,0 +1,11 @@
+import { $where } from "../../backend/models/groupModel";
+
+(function() {
+    var socket = io();
+    $("form").submit(function(e) {
+        e.preventDefault();
+        socket.emit("chat message", $("#m").val());
+        $("#m").val("");
+        return true;
+    })
+})
