8b612ea4ef91d8c63908767ff85cae2e82c1d98f
Ksenia Velichko <ksushi@Ksenias-MacBook-Pro.local>
2022-04-15 14:34:35 -0400

    group buttons + trashcan logo

--- frontend/src/components/GroupItem.jsx
+++ frontend/src/components/GroupItem.jsx
@@ -1,13 +1,25 @@
 import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import trash  from '../trashcan.png'
 import { deleteGroup } from '../features/groups/groupSlice'
+
 
 function GroupItem({ group }) {
   const dispatch = useDispatch()
+  let navigate = useNavigate();
+const routeChange = () =>{
+  let path = '/groups';
+  navigate(path);
+}
   return (
     <div className='group'>
-      <h2>{group.text}</h2>
+      <button className='classbtn' onClick={routeChange}>
+        <h2>
+          {group.text}
+        </h2>
+      </button>
       <button onClick={() => dispatch(deleteGroup(group._id))} className='close'>
-        X
+        <img src={trash} height={10} width={10}></img>
       </button>
     </div>
   )
