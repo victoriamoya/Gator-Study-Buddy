be793ca29a4d8bfc3e9a4c2c47e7f90d5dfee2ed
sammy-soto10 <73593202+sammy-soto10@users.noreply.github.com>
2022-04-15 19:01:27 -0400

    fix user name to user

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -38,7 +38,7 @@ function Dashboard() {
   return (
     <>
       <section className='heading'>
-        <h1>Welcome {user && user.name}</h1>
+        <h1>Welcome {user.name}</h1>
         <p>Groups Dashboard</p>
       </section>
 
