49b914245e1285af7bb1a9d45c968be7534f7d91
sammy-soto10 <73593202+sammy-soto10@users.noreply.github.com>
2022-04-17 21:07:27 -0400

    fix sign out button for chat page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
   return (
     <header className='header'>
       <div className='logo'>
-        <Link to='/'>StudyBuddies</Link>
+        <Link to='/login'>StudyBuddies</Link>
       </div>
       <ul>
         {user ? (
